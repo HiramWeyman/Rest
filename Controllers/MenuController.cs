25edc9470175039595a0ff739a50bc2d5ca28d04
Hiram <hiram.wv@gmail.com>
2020-04-23 21:52:03 -0500

    se agrego mas consultas al menucontroller

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -21,15 +21,76 @@ namespace Rest.Controllers
             }
         }
 
-        // GET: api/Menu/5
-        public IEnumerable<Menu> Get(int id)
+        public HttpResponseMessage Get(String Bandera)
         {
             using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
             {
                 db.Configuration.LazyLoadingEnabled = false;
-                return db.Menus.Where(x => x.menu_id == id).ToList();
+                var menu = db.Menus.Where(x => x.menu_orden != 999).OrderBy(x => x.menu_orden).ToList();
+                if (menu != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, menu);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Menu no encontrado.");
+                }
+            }
+        }
+
+        // GET: api/Menu/5
+        //public IEnumerable<Menu> Get(int id)
+        public HttpResponseMessage Get(int id)
+        {
+            using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                var menu = db.Menus.FirstOrDefault(x => x.menu_id == id);
+                if (menu != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, menu);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Menu no encontrado.");
+                }
 
             }
         }
+
+        [HttpPut]
+        public HttpResponseMessage Put(int id, MenuCLS menuCLS)
+        {
+
+            try
+            {
+                id = menuCLS.menu_id;
+                using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+                {
+                    Menu menu = db.Menus.Where(p => p.menu_id.Equals(id)).First();
+                    if (menu == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Menu no encontrado.");
+                    }
+                    else
+                    {
+                        menu.menu_descrip = menuCLS.menu_descrip;
+                        menu.menu_orden = menuCLS.menu_orden;
+                        menu.menu_cancelar = menuCLS.menu_cancelar;
+                        db.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
     }
 }
--- Controllers/UsuariosController.cs
+++ Controllers/UsuariosController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -103,7 +103,7 @@ namespace Rest.Controllers
 
         }
 
-        [HttpPut]
+
         public HttpResponseMessage Put(int id,UsuariosCLS userCLS)
         {
 
