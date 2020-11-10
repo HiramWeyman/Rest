3885565efbdb071e78518166b95cc89f1fef901f
Hiram <hiram.wv@gmail.com>
2020-11-10 12:48:49 -0600

    solicitud revolvente

---
+++ Controllers/PreRevolventeController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Rest.Models;
+using System.Net.Http;
+using System.Net;
+
+namespace Rest.Controllers
+{
+    public class PreRevolventeController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Pre_Revolvente> Get()
+        {
+            using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                return db.Pre_Revolvente.OrderByDescending(x => x.pr_id).ToList();
+
+            }
+        }
+
+        public IEnumerable<Pre_Revolvente> Get(string matricula)
+        {
+            using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+            {
+                db.Configuration.LazyLoadingEnabled = false;
+                return db.Pre_Revolvente.Where(x => x.pr_matricula == matricula).OrderByDescending(x => x.pr_id).ToList();
+            }
+        }
+
+        public HttpResponseMessage Post(string matricula, string nombre, RevolventeCLS revolventeCLS)
+        {
+
+            try
+            {
+                using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+                {
+
+                    Pre_Revolvente pre_revolvente = new Pre_Revolvente();
+
+                    pre_revolvente.pr_nombre = nombre;
+                    pre_revolvente.pr_matricula = matricula;
+                    pre_revolvente.pr_telefono = revolventeCLS.pr_telefono;
+                    pre_revolvente.pr_ingreso = revolventeCLS.pr_ingreso;
+                    pre_revolvente.pr_modificacion = revolventeCLS.pr_modificacion;
+                    pre_revolvente.pr_tipo = revolventeCLS.pr_tipo;
+                    pre_revolvente.pr_estatus = "PENDIENTE";
+                    pre_revolvente.pr_fecha = DateTime.Now;
+
+                    db.Pre_Revolvente.Add(pre_revolvente);
+                    db.SaveChanges();
+                    var Mensaje = Request.CreateResponse(HttpStatusCode.Created, revolventeCLS);
+                    return Mensaje;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+        [HttpPut]
+        public HttpResponseMessage Put(int id, string valor)
+        {
+
+            try
+            {
+                //id = cajaahorroCLS.pre_id;
+                using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+                {
+                    Pre_Revolvente revolvente = db.Pre_Revolvente.Where(p => p.pr_id.Equals(id)).First();
+                    if (revolvente == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Prestamo no encontrado");
+                    }
+                    else
+                    {
+                        revolvente.pr_estatus = valor;
+                        db.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK);
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+    }
+}
---
+++ Models/RevolventeCLS.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Rest.Models
+{
+    public class RevolventeCLS
+    {
+        [Key]
+        public int pr_id { get; set; }
+        public string pr_nombre { get; set; }
+        public string pr_matricula { get; set; }
+        public string pr_telefono { get; set; }
+        public string pr_ingreso { get; set; }
+        public string pr_modificacion { get; set; }
+        public string pr_tipo { get; set; }
+        public string pr_estatus { get; set; }
+        public string pr_recibo { get; set; }
+        public string pr_ine { get; set; }
+        public System.DateTime pr_fecha { get; set; }
+
+    }
+}
