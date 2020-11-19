33b0613dd612a49091558cefba36a261f602c153
Hiram <hiram.wv@gmail.com>
2020-11-19 17:22:32 -0600

    funcion de recuperar password

--- Controllers/UsersBaseController.cs
+++ Controllers/UsersBaseController.cs
@@ -22,6 +22,134 @@ namespace Rest.Controllers
             }
         }
 
+        [Route("api/getUserBase")]
+        [HttpGet]
+        public HttpResponseMessage GetUserBase()
+        {
+
+            try
+            {
+
+                using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+                {
+
+                    List<ComprobarCLS> usuario = null;
+                    //ComprobarCLS usuario = new ComprobarCLS();
+                    string query = " SELECT [ub_id],[ub_user],[ub_nombre],[ub_curp],[ub_rfc]  FROM [steujedo_sindicato].[steujedo_sindicato].[User_Base] order by [ub_nombre] ";
+                    usuario = db.Database.SqlQuery<ComprobarCLS>(query).ToList();
+                    //usuario = db.User_Base.FirstOrDefault(e => e.ub_user == matricula && e.ub_curp==curp && e.ub_rfc==rfc);
+                    if (usuario != null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, usuario);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Trabajador  no encontrado");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+
+        [Route("api/getUserBaseMat")]
+        [HttpGet]
+        public HttpResponseMessage GetUserBaseMat([FromUri]string matricula)
+        {
+
+            try
+            {
+
+                using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+                {
+
+                    List<ComprobarCLS> usuario = null;
+                    //ComprobarCLS usuario = new ComprobarCLS();
+                    string query = " SELECT [ub_id],[ub_user],[ub_nombre],[ub_curp],[ub_rfc]  FROM [steujedo_sindicato].[steujedo_sindicato].[User_Base] where [ub_user]="+matricula;
+                    usuario = db.Database.SqlQuery<ComprobarCLS>(query).ToList();
+                    //usuario = db.User_Base.FirstOrDefault(e => e.ub_user == matricula && e.ub_curp==curp && e.ub_rfc==rfc);
+                    if (usuario != null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, usuario);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Trabajador  no encontrado");
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+        [Route("api/updateuserbase/{user}")]
+        [HttpPut]
+        public HttpResponseMessage PutBaseUser(string user, UserBaseCLS userCLS)
+        {
+
+            try
+            {
+                user = userCLS.ub_user;
+                using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
+                {
+                    User_Base usuarios = new User_Base();
+                    usuarios = db.User_Base.Where(p => p.ub_user.Equals(user)).First();
+                    if (usuarios == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Trabajador con ID " + user.ToString() + " no encontrado");
+                    }
+                    else
+                    {
+                      
+                        if (userCLS.ub_password != null && userCLS.ub_password != "")
+                        {
+
+                            usuarios.ub_nombre = userCLS.ub_nombre;
+                            usuarios.ub_curp = userCLS.ub_curp;
+                            usuarios.ub_rfc = userCLS.ub_rfc;
+                            SHA256Managed sha = new SHA256Managed();
+                            byte[] byteContra = Encoding.Default.GetBytes(userCLS.ub_password);
+                            byte[] byteContraCifrado = sha.ComputeHash(byteContra);
+                            string contraCifrada = BitConverter.ToString(byteContraCifrado).Replace("-", "");
+                            usuarios.ub_password = contraCifrada;
+                         
+                            db.SaveChanges();
+                            return Request.CreateResponse(HttpStatusCode.OK);
+                        }
+                        else
+                        {
+                            usuarios.ub_nombre = userCLS.ub_nombre;
+                            usuarios.ub_curp = userCLS.ub_curp;
+                            usuarios.ub_rfc = userCLS.ub_rfc;
+                            db.SaveChanges();
+                            return Request.CreateResponse(HttpStatusCode.OK);
+                        }
+
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
+
+
         public HttpResponseMessage Get(string Usuario)
         {
             using (steujedo_sindicatoEntities db = new steujedo_sindicatoEntities())
---
+++ Models/ComprobarCLS.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rest.Models
+{
+    public class ComprobarCLS
+    {
+
+        public int ub_id { get; set; }
+        public string ub_user { get; set; }
+        public string ub_nombre { get; set; }
+        public string ub_curp { get; set; }
+        public string ub_rfc { get; set; }
+    }
+}
